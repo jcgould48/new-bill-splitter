a887f462c4b527d04309f4c94d9086344e4c6fd0
Jesse Gould <jesse.gould@codeimmersives.com>
2019-10-30 12:01:04 -0400

    more dynamic with inputs

--- bill-split-calculator.js
+++ bill-split-calculator.js
@@ -5,10 +5,10 @@ function billSplitCalc(bill, tipPercentage, numberOfSplits) {
     if (tipPercentage > 0) {
         let withTip = bill * (1+tipConverter(tipPercentage));
         
-        return '$' + (total = withTip / numberOfSplits) + reaction(bill , numberOfSplits);
+        return '$' + (total = withTip / splitConverter(numberOfSplits)) + reaction(bill , splitConverter(numberOfSplits));
 
     } else {
-        return `$${total = bill / numberOfSplits} each`;
+        return `$${total = bill / splitConverter(numberOfSplits)} each`;
     }}
 
 
@@ -26,7 +26,7 @@ function tipConverter(tipPercentage){
 function reaction(bill, numberOfSplits) {
     if (bill >200 && numberOfSplits > 1){
         return (" per person. Daaaamn, you have expensive friends!")
-        }else if (bill<5){
+        }else if (bill<5 && numberOfSplits > 1){
             return (" per person. Be the hero and just cover the whole thing.")
         } else if (isNaN(bill)){
             return ('Numbers are hard');
@@ -38,36 +38,14 @@ function reaction(bill, numberOfSplits) {
 }
 
 
-// function splitConverter(numberOfSplits){
-//     if (numberOfSplits === undefined || numberOfSplits===0){
-//     return 1;
-// }else {
-//     return numberOfSplits;
-// }
-// }
+function splitConverter(numberOfSplits){
+    if (numberOfSplits === undefined || numberOfSplits===0){
+    return 1;
+}else {
+    return numberOfSplits;
+}
+}
 
-// function billSplitter(bill, tipPercentage, numberOfSplits){
-//     const billTotal = bill * (1 + tipConverter(tipPercentage));
-//     const perPersonTotal =  billTotal / splitConverter(numberOfSplits);
-//     return (perPersonTotal);
-//     }
-   
-
-// const personalBill = billSplitter(bill, tipPercentage, numberOfSplits);
-
-// if (personalBill >200 && numberOfSplits > 1){
-//     console.log('$' + personalBill + " per person. Daaaamn, you have expensive friends!")
-// }else if (personalBill<5){
-//     console.log('$' + personalBill + " per person. Be the hero and just cover the whole thing.")
-// } else if (isNaN(personalBill)){
-//     console.log('Numbers are hard');
-// } 
-// else
-// {
-//     console.log('$' + personalBill + " per person.");
-// }
-
-// }
 
 
 // function billSplitCalc(bill, tipPercentage, numberOfSplits) {
